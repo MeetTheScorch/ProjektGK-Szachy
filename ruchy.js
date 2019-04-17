db48749fe9fd08bed5a608209f45902bcbcddd62
MeetTheScorch <scorch.pl@gmail.com>
2019-04-17 18:25:13 +0200

    Szachy-ProjektGK

---
+++ ruchy.js
@@ -0,0 +1,8 @@
+var ruchy = [
+				[{first:"E", second:2}, {first:"E", second:4}],
+				[{first:"D", second:7}, {first:"D", second:5}],
+				[{first:"E", second:4}, {first:"D", second:5}],
+				[{first:"G", second:8}, {first:"F", second:6}],
+				[{first:"F", second:1}, {first:"B", second:5}],
+				[{first:"F", second:6}, {first:"D", second:7}],
+			];
